0b83a34212ddf19f76a2b852362b35dbe08bf387
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-01 13:16:57 -0700

    added more subscriptions to list

--- src/containers/Sidebar/Subscriptions/Subscriptions.js
+++ src/containers/Sidebar/Subscriptions/Subscriptions.js
@@ -8,8 +8,13 @@ export default function Subscriptions() {
     <>
       <SidebarHeader title="Subscriptions" />
       <Subscription label="Diversity" broadcasting />
+      <Subscription label="Pop Music - Topic" newVids={121} />
+      <Subscription label="Blume" newVids={8} />
       <Subscription label="NoCopyrightSounds" newVids={5} />
-
+      <Subscription label="Nightblue Music" newVids={11} />
+      <Subscription label="DØVYDAS" newVids={2} />
+      <Subscription label="Chill Nation" newVids={33} />
+      <Subscription label="CS50" />
       <Divider />
     </>
   );
