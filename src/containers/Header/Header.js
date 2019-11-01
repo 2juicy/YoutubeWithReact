2de680f2893e6dd7a064d565b4323572f7c07a50
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-01 11:26:14 -0700

    light styling for pixel perfection

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,16 +1,39 @@
 import React from "react";
-import { Menu, Image } from "semantic-ui-react";
+import { Menu, Image, Form, Input, Icon } from "semantic-ui-react";
 import "./Header.scss";
 
 export default function Header() {
   return (
     <Menu borderless className="top-menu" fixed="top">
       <Menu.Item header className="logo">
-        <Image src={process.env.PUBLIC_URL + "/YourTube.png"} size="tiny" />
-        <h1>YourTube</h1>
+        <Image src={process.env.PUBLIC_URL + "/YourTube.png"} size="mini" />
+        <h2>YourTube</h2>
       </Menu.Item>
       <Menu.Menu className="nav-container">
-        klawjdklwajkldwajkldawjwda
+        <Menu.Item className="search-input">
+          <Form>
+            <Form.Field>
+              <Input placeholder="Search" size="small" action="Go" />
+            </Form.Field>
+          </Form>
+        </Menu.Item>
+      </Menu.Menu>
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Icon className="header-icon" name="video camera" size="large" />
+        </Menu.Item>
+        <Menu.Item>
+          <Icon className="header-icon" name="grid layout" size="large" />
+        </Menu.Item>
+        <Menu.Item>
+          <Icon className="header-icon" name="alarm" size="large" />
+        </Menu.Item>
+        <Menu.Item name="avatar">
+          <Image
+            src="https://yt3.ggpht.com/a-/AAuE7mBQq0sOlxUxcp71beNsH8PsR0WyTPbc1S6cJA=s88-c-k-c0x00ffffff-no-rj-mo"
+            avatar
+          />
+        </Menu.Item>
       </Menu.Menu>
     </Menu>
   );
