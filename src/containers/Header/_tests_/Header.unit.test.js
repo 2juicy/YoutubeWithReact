ac68e3cee60207e5f85382de8bd1468874b10307
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-02 09:01:55 -0700

    created first unit test, fixed naming

--- src/containers/Header/_tests_/Header.unit.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Header from "../Header";
-
-test("renders Header", () => {
-  const wrapper = shallow(<Header />);
-  expect(wrapper).toMatchSnapShot();
-});
