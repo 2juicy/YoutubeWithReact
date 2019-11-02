880e89c4cd2f0507a7965faa79a3a023cd4c5681
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-02 11:11:46 -0700

    vid preview component

--- src/components/VidPreview/VidPreview.js
+++ src/components/VidPreview/VidPreview.js
@@ -1,6 +1,26 @@
 import React from "react";
 import "./VidPreview.scss";
+import { Image } from "semantic-ui-react";
 
 export default function VidPreview() {
-  return <div>kjawhdjkahdjwahdaw placeholder</div>;
+  return (
+    <div className="vid-preview">
+      <div className="image-container">
+        <Image src="http://via.placeholder.com/210x118" />
+        <div className="time-label">
+          <span>04:20</span>
+        </div>
+      </div>
+
+      <div className="vid-info">
+        <div className="semi-bold show-max-two-lines">Video Title</div>
+        <div className="vid-preview-metadata-container">
+          <div className="channel-title">Channel Title</div>
+          <div>
+            <span>24M views ⚬ 22 days ago</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
