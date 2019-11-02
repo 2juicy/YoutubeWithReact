91978a2548200946df5b9e8cee221f2eb787d42d
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-02 09:51:51 -0700

    rewrite unit test

--- src/containers/Sidebar/SidebarItem/__tests__/SidebarItem.unit.test.js
+++ src/containers/Sidebar/SidebarItem/__tests__/SidebarItem.unit.test.js
@@ -2,14 +2,31 @@ import React from "react";
 import { shallow } from "enzyme";
 import SidebarItem from "../SidebarItem";
 
+const location = { pathname: "/feed/trending" };
+
 describe("SidebarItem", () => {
-  test("renders empty SidebarItem", () => {
-    const wrapper = shallow(<SidebarItem />);
+  test("renders empty SidebarItem without path", () => {
+    const wrapper = shallow(<SidebarItem location={location} />);
     expect(wrapper).toMatchSnapshot();
   });
-});
 
-test("renders highlighted SideBarItem that navigates to /feed/trending", () => {
-  const wrapper = shallow(<SidebarItem label="Trending" icon="fire" />);
-  expect(wrapper).toMatchSnapshot();
+  test("renders highlighted SideBarItem", () => {
+    const wrapper = shallow(
+      <SidebarItem
+        highlighted
+        label="Trending"
+        icon="fire"
+        location={location}
+        path={"/feed/trending"}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("render non-highlighted SideBarItem", () => {
+    const wrapper = shallow(
+      <SidebarItem label="Trending" icon="fire" location={location} />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
 });
