45e9b3ccd824795147e3dac34c49464db6b6ff50
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-05 08:25:21 -0800

    link to home page

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Menu, Image, Form, Input, Icon } from "semantic-ui-react";
 import "./Header.scss";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <Menu borderless className="top-menu" fixed="top">
       <Menu.Item header className="logo">
-        <Image src={process.env.PUBLIC_URL + "/YourTube.png"} size="mini" />
-        <h2>YourTube</h2>
+        <Link to="/">
+          <Image src={process.env.PUBLIC_URL + "/YourTube.png"} size="mini" />
+        </Link>
+        <Link to="/">
+          <h2>YourTube</h2>
+        </Link>
       </Menu.Item>
+
       <Menu.Menu className="nav-container">
         <Menu.Item className="search-input">
           <Form>
