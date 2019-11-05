73d6ad4bb6b16b5b51d90b54aaeae171b493d26f
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-05 08:02:26 -0800

    added simple routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
 import Home from "./containers/Home/Home";
+import { Switch, Route } from "react-router-dom";
+import Watch from "./containers/Watch/Watch";
 
 export default function App() {
   return (
     <Layout>
-      <Home />
+      <Switch>
+        <Route path="/watch" component={Watch} />
+        <Route path="/" component={Home} />
+      </Switch>
     </Layout>
   );
 }
