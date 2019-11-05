a69704cf09fcfefd82e333e897ec78d71a12e1fe
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-05 14:22:38 -0800

    fixed problems with grid

--- src/components/VidMetaData/VidMetaData.js
+++ src/components/VidMetaData/VidMetaData.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./VidMetaData.scss";
-import { Divider } from "semantic-ui-react";
+import { Divider, Button, Icon } from "semantic-ui-react";
+import Rating from "../Rating/Rating";
 
 export default function VidMetaData({ views }) {
   return (
@@ -8,6 +9,19 @@ export default function VidMetaData({ views }) {
       <h3>Video Title</h3>
       <div className="vid-stats">
         <span>{views} views</span>
+        <div className="vid-actions">
+          <Rating likes={123} dislikes={12} />
+          <Button basic icon labelPosition="left">
+            <Icon name="share" />
+            Share
+          </Button>
+          <Button basic icon>
+            <Icon name="add circle" />
+          </Button>
+          <Button basic icon>
+            <Icon name="ellipsis horizontal" />
+          </Button>
+        </div>
       </div>
       <Divider />
     </div>
