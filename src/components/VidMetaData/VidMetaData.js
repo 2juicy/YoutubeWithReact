caee073375e81f6980867a5be5707affcd1c976f
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-05 14:57:18 -0800

    pixel perfect styling

--- src/components/VidMetaData/VidMetaData.js
+++ src/components/VidMetaData/VidMetaData.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./VidMetaData.scss";
-import { Divider, Button, Icon } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
 import Rating from "../Rating/Rating";
 
 export default function VidMetaData({ views }) {
+  const commas = views.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
   return (
     <div className="vid-metadata">
       <h3>Video Title</h3>
       <div className="vid-stats">
-        <span>{views} views</span>
+        <span>{commas} views</span>
         <div className="vid-actions">
           <Rating likes={123} dislikes={12} />
           <Button basic icon labelPosition="left">
@@ -23,7 +25,6 @@ export default function VidMetaData({ views }) {
           </Button>
         </div>
       </div>
-      <Divider />
     </div>
   );
 }
