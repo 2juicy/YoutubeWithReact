5443d02fa2966df13c3bf0cb6e0f1823a0f73d8f
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-05 08:49:30 -0800

    tests updated

---
+++ src/__tests__/App.unit.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App";
+
+describe("App", () => {
+  test("renders", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
