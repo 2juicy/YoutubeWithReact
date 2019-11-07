7e187f8daf01e51b7d46ea6efe5cea8e06de841d
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-07 07:31:35 -0800

    add comment component started

---
+++ src/containers/Comments/AddComment/AddComment.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./AddComment.scss";
+import { Form, Image } from "semantic-ui-react";
+
+export default function AddComment() {
+  return (
+    <div className="add-comment">
+      <Image
+        className="user-image"
+        src="http://via.placeholder.com/48x48"
+        circular
+      />
+      <Form>
+        <Form.TextArea
+          control={TextArea}
+          autoHeight
+          placeholder="Add a public comment"
+        />
+      </Form>
+    </div>
+  );
+}
