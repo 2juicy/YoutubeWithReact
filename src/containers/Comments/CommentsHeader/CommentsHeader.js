8e35e7160d145ff6d52d57eade14074a6d0967e7
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-06 16:32:49 -0800

    added comments to watch container

--- src/containers/Comments/CommentsHeader/CommentsHeader.js
+++ src/containers/Comments/CommentsHeader/CommentsHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Comments.scss";
+import "./CommentsHeader.scss";
 import { Button, Icon } from "semantic-ui-react";
 
 export default function Comments({ amount }) {
--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -4,6 +4,7 @@ import Video from "../../components/Video/Video";
 import RelatedVids from "../../components/RelatedVids/RelatedVids";
 import VidMetaData from "../../components/VidMetaData/VidMetaData";
 import VidInfoBox from "../../components/VidInfoBox/VidInfoBox";
+import Comments from "../Comments/Comments";
 
 export default function Watch() {
   return (
@@ -11,12 +12,7 @@ export default function Watch() {
       <Video className="video" id="L14hxW2SN68" />
       <VidMetaData className="metadata" views={9999} />
       <VidInfoBox />
-      <div
-        className="comments"
-        style={{ width: "100%", height: "100px", backgroundColor: "blue" }}
-      >
-        Comments
-      </div>
+      <Comments />
       <RelatedVids />
     </div>
   );
