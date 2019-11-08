a42430a1e8d55f736db63991bcceece0371e1aab
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-07 17:20:24 -0800

    unit test

---
+++ src/containers/Comments/AddComment/__tests__/AddComment.unit.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow } from "enzyme";
+import AddComment from "../AddComment";
+
+describe("AddComment", () => {
+  test("renders", () => {
+    const wrapper = shallow(<AddComment />);
+    expect(wrapper).toMatchSnapShot();
+  });
+});
