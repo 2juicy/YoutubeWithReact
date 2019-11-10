995e6ba53a63dcad368a6d9f02d822fbfe99fedb
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-10 15:29:28 -0800

    devtools setup

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,15 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import reducer from "./reducers";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas";
 
 export function configureStore() {
   const sagaMiddleware = createSagaMiddleware;
-  const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+  const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+  const store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(sagaMiddleware))
+  );
+  sagaMiddleware.run(rootSaga);
+  return store;
 }
