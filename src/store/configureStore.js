dd1f363eed42d75d416298bee72d9b6b0e5ba115
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-10 15:27:53 -0800

    naming

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
 import reducer from "./reducers";
 import createSagaMiddleware from "redux-saga";
-import moduleName from "./sagas";
+import rootSaga from "./sagas";
 
 export function configureStore() {
   const sagaMiddleware = createSagaMiddleware;
