13dff319fac176c31e9040d229d2c867e525e11b
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-10 15:32:17 -0800

    redux + middleware setup done

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import App from "./App";
 import "semantic-ui-css/semantic.min.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./store/configureStore";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,7 +4,7 @@ import createSagaMiddleware from "redux-saga";
 import rootSaga from "./sagas";
 
 export function configureStore() {
-  const sagaMiddleware = createSagaMiddleware;
+  const sagaMiddleware = createSagaMiddleware();
   const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
   const store = createStore(
     reducer,
