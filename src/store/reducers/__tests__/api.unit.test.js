b95bc177bf8560cf3d9e5c33896a195ee0071ea1
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-12 14:26:09 -0800

    tests written

--- src/store/reducers/__tests__/api.unit.test.js
+++ src/store/reducers/__tests__/api.unit.test.js
@@ -13,4 +13,23 @@ describe("api reducer", () => {
       expectedEndState
     );
   });
+
+  test("test api reducer with YOUTUBE_LIBRARY_LOADED action", () => {
+    const startState = { ...initialState };
+    const expectedEndState = {
+      libraryLoaded: true
+    };
+    expect(apiReducer(startState, { type: YOUTUBE_LIBRARY_LOADED })).toEqual(
+      expectedEndState
+    );
+  });
+
+  test("test api reducer for idempotence with YOUTUBE_LIBRARY_LOADED action and library already looted", () => {
+    const startState = {
+      libraryLoaded: true
+    };
+    expect(apiReducer(startState, { type: YOUTUBE_LIBRARY_LOADED })).toEqual(
+      startState
+    );
+  });
 });
