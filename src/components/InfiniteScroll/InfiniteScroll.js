728d5a368123dea0f364fbcb68232341a4ce51c0
JohnnyC <j0hnnywchiang@gmail.com>
2019-11-28 17:59:53 -0800

    started on infinite scroll container

---
+++ src/components/InfiniteScroll/InfiniteScroll.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./InfiniteScroll.scss";
+import Waypoint from "react-waypoint";
+import { Loader } from "semantic-ui-react";
+
+export default function InfiniteScroll(props) {
+  return (
+    <>
+      {props.children}
+      <Waypoint onEnter={props.bottomReachedCallback}>
+        <div className="loader-container">
+          <Loader active={props.showLoader} inline="centered" />
+        </div>
+      </Waypoint>
+    </>
+  );
+}
