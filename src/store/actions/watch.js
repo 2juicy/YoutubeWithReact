81866d16f4ce7376b005f092fc0f944c434b8107
JohnnyC <j0hnnywchiang@gmail.com>
2019-12-06 05:45:24 -0800

    boilerplate for vid detail selectors

---
+++ src/store/actions/watch.js
@@ -0,0 +1,15 @@
+import {
+  createAction,
+  createRequestTypes,
+  REQUEST,
+  SUCCESS,
+  FAILURE
+} from "./index";
+
+export const WATCH_DETAILS = createRequestTypes("WATCH_DETAILS");
+
+export const details = {
+  request: videoId => createAction(WATCH_DETAILS[REQUEST], { videoId }),
+  success: response => createAction(WATCH_DETAILS[SUCCESS], { response }),
+  failure: response => createAction(WATCH_DETAILS[FAILURE], { response })
+};
