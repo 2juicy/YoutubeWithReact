1d30593b8c9eb8aec2c68c899d0aa82a3aba85cd
JohnnyC <j0hnnywchiang@gmail.com>
2019-12-23 15:51:38 -0800

    rating component fixed

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -1,25 +1,35 @@
 import React from "react";
 import "./Rating.scss";
 import { Icon, Progress } from "semantic-ui-react";
+import { getShortNumberString } from "../../services/number/number-format";
 
-export default function Rating({ likes, dislikes, hideProgress }) {
-  const percent = likes && dislikes ? 100 * (likes / (likes + dislikes)) : null;
-  console.log(percent);
+export default function Rating({ likes, dislikes }) {
+  let percent = null;
+  let likeCount = likes !== 0 ? likes : null;
+  let dislikeCount = null;
+
+  if (likes && dislikes) {
+    const amountLikes = parseInt(likes);
+    const amountDislikes = parseInt(dislikes);
+    const ratingPercent = 100 * (amountLikes / (amountLikes + amountDislikes));
+
+    percent = <Progress percent={ratingPercent} size="tiny" />;
+    likeCount = getShortNumberString(amountLikes);
+    dislikeCount = getShortNumberString(amountDislikes);
+  }
 
   return (
     <div className="rating">
       <div className="thumbs-up">
         <Icon name="thumbs outline up" />
-        <span>{likes}</span>
+        <span>{likeCount}</span>
       </div>
 
       <div className="thumbs-down">
         <Icon name="thumbs outline down" />
-        <span>{dislikes}</span>
+        <span>{dislikeCount}</span>
       </div>
-      {percent && !hideProgress ? (
-        <Progress className="progress" percent={percent} size="tiny" />
-      ) : null}
+      {percent}
     </div>
   );
 }
